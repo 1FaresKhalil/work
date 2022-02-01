b970f64095b1e77e7d76c0714dd8e51af581be37
1fareskhalil <fareskhalil555@outlook.com>
2022-02-01 17:45:58 +0200

    h

---
+++ src/constant.js
@@ -0,0 +1,45 @@
+import dress_1_front from "./assets/images/BLACK DRESS.jpg";
+import dress_1_detail from "./assets/videos/item.webm";
+import dress_1_video from "./assets/videos/model.webm";
+import dress_1_runway from "./assets/videos/runaway.webm";
+
+import dress_2_front from "./assets/images/RED DRESS.jpg";
+import dress_2_detail from "./assets/videos/dress_2.mp4";
+// import dress_2_video from './assets/videos/video_2.mp4';
+// import dress_2_runway from './assets/videos/runway_2.mp4';
+
+import dress_3_front from "./assets/images/ORANGE DRESS.jpg";
+import dress_3_detail from "./assets/videos/dress_3.MP4";
+
+export const Dresses = [
+  {
+    id: 1,
+    name: "Chenille evening dress with a slit",
+    price: "$ 2,195",
+    color: "Navy",
+    front: dress_1_front,
+    detail: dress_1_detail,
+    video: dress_1_video,
+    runway: dress_1_runway,
+  },
+  {
+    id: 2,
+    name: "Flowing gabardine tunic dress with front panel",
+    price: "$ 695",
+    color: "Navy",
+    front: dress_2_front,
+    detail: dress_2_detail,
+    video: dress_1_video,
+    runway: dress_1_runway,
+  },
+  {
+    id: 3,
+    name: "Knit dress with jacquard zigzag motif",
+    price: "$ 645",
+    color: "INDIGO",
+    front: dress_3_front,
+    detail: dress_3_detail,
+    video: dress_1_video,
+    runway: dress_1_runway,
+  },
+];
